00e5e181a5b56987d6f6a4c0aab79a28a12cf124
Mike <michael.russell.ut@gmail.com>
2019-04-03 12:33:07 -0600

    initial upload

---
+++ venv/GradingSubmissions/AnotherTester.java
@@ -0,0 +1,10 @@
+/* 
+* Here is an intro to this program...
+* Author: Mike R
+*/
+
+public class AnotherTester{
+	public static void main(String[] args){
+		System.out.println("Hello world this is awesome\n   and a new line!");
+	}
+}
---
+++ venv/GradingSubmissions/TestFile.java
@@ -0,0 +1,13 @@
+/* 
+* Here is an intro to this program...
+* Author: Mike R
+*/
+
+public class TestFile{
+	// This is a class that holds simple statements
+	public static void main(String[] args){
+		// This is a comment to show I am printing
+		double a = 1;a;f 
+		System.out.println(a + "Hello world this is awesome\n   and a new line!");
+	}
+}
